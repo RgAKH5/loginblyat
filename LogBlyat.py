0e2fcaa82a4367529983457563b233fb63dfa867
RgAKH5 <56658138+RgAKH5@users.noreply.github.com>
2019-12-03 19:52:40 +0700

    Add files via upload
    Cuman Iseng semata :v

---
+++ LogBlyat.py
@@ -0,0 +1,5 @@
+print("hello world")
+print("windroid 1.1 corp® taon 2019-an:v")
+print("dibuat dari hp xiaomay redmi note 5A codename: ugglite")
+nama_lu = input("blyat : ")
+print("Hello "+ nama_lu)
